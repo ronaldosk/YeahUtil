e253041e7dc89a6508c4683e1d2fad3bf21516c2
ronaldosk <ronaldosk@outlook.com>
2018-11-02 16:37:59 +0800

    更新模板文件

--- CodeGenerate/Template/Application/APP/Dtos/GetAllTasksInput.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Acme.SimpleTaskApp.Tasks.Dtos
-{
-    public class GetAllTasksInput
-    {
-        public TaskState? State { get; set; }
-    }
-}
---
+++ CodeGenerate/Template/Const.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGenerate.Template
+{
+    public class Const
+    {
+        public static string appName = "[AppName]";//namespace 
+        public static string businessName = "[BusinessName]";
+        public static string businessNameLow = "[businessName]";//business实例的名称，规则是首字母小写
+    }
+}
