6aa750a00a7c0256856c10b43831b581d964bdf0
ronaldosk <ronaldosk@outlook.com>
2018-11-20 15:48:57 +0800

    Element-Ui 使用 cdn https://github.com/PanJiaChen/vue-admin-template/blob/master/README-zh.md

--- vueadmintemplate/src/store/index.js
+++ vueadmintemplate/src/store/index.js
@@ -1,11 +1,11 @@
-import Vue from 'vue'
+//import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './modules/app'
 import user from './modules/user'
 import permission from './modules/permission'
 import getters from './getters'
 
-Vue.use(Vuex)
+//Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
